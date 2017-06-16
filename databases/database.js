3df7c81ed3196f1f438492f7941ea0d05fa1e5cb
dale <dale.guo@mullenloweprofero.com>
2017-06-16 18:17:49 +0800

    ok for docker

--- databases/database.js
+++ databases/database.js
@@ -1,15 +1,15 @@
 // getting-started.js
+var debug = require('debug')('database');
 var mongoose = require('mongoose');
 var connect_str = 'mongodb://localhost:27017/db';
 if(process.env.MONGO_PORT_27017_TCP_ADDR){
   var connect_str = 'mongodb://'+process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/'+process.env.MONGO_INSTANCE_NAME;
-  console.log('connect_str');
 }
 mongoose.connect(connect_str);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('were connected!');
+	debug(connect_str+' : connected!');
 });
 // var mongo = require('./databases/database');
 
